5a8e8214566f2e5ffadc0a4d1748472dfdc89c2d
Babz-G <barbaramgaynor@gmail.com>
2026-03-19 15:35:23 -0400

    built out homepage with food truck cards

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react";
 import "../App.css";
 
 function Home() {
-  return <></>;
+  const [trucks, setTrucks] = useState([]);
+
+  useEffect(() => {
+    async function fetchTrucks() {
+      const response = await fetch("/api/get-all-food-trucks");
+      const data = await response.json();
+      setTrucks(data);
+    }
+    fetchTrucks();
+  }, []);
+
+  return (
+    <>
+      <h1>All Food Trucks</h1>
+      <h2>Total number of food trucks: {trucks.length}</h2>
+
+      <div className="card-container">
+        {trucks.map((truck) => (
+          <div className="card" key={truck.id}>
+            <h2>{truck.name}</h2>
+            <p>Id: {truck.id}</p>
+            <p>Location: {truck.current_location}</p>
+            <p>Daily Special: {truck.daily_special}</p>
+            <p>Slogan: {truck.slogan}</p>
+            <p>
+              Has Vegan Options: {truck.has_vegan_options ? "Yes ✅" : "No ❌"}
+            </p>
+            <p>Price Level: {truck.price_level}</p>
+            <p>Rating: {truck.rating}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Home;
